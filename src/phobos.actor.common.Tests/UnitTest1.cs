358b600696bf24f05993990f729ed9be02bfd6f9
Aaron Stannard <aaron@petabridge.com>
2018-03-08 12:25:52 -0600

    initial commit

---
+++ src/phobos.actor.common.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using System;
+using Xunit;
+
+namespace Phobos.Actor.Common.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void TestMethod1()
+        {
+        }
+    }
+}
