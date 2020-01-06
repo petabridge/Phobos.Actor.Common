5c2ace7c57d06b3a7c04b770653cfa5e47a63279
Aaron Stannard <aaron@petabridge.com>
2020-01-06 12:42:22 -0600

    Explicit trace targeting interfaces (#23)
    * add ITrace/IMonitor/IInstrumeted interfaces
    
    * added INeverInstrumented convenience interface

---
+++ src/Phobos.Actor.Common/IInstrumented.cs
@@ -0,0 +1,15 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="IInstrumented.cs" company="Petabridge, LLC">
+//      Copyright (C) 2015 - 2020 Petabridge, LLC <https://petabridge.com>
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Phobos.Actor.Common
+{
+    /// <summary>
+    ///     Marker interface that indicates that this resource is fully instrumented.
+    /// </summary>
+    public interface IInstrumented : ITrace, IMonitor
+    {
+    }
+}
---
+++ src/Phobos.Actor.Common/IMonitor.cs
@@ -0,0 +1,15 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="IMonitor.cs" company="Petabridge, LLC">
+//      Copyright (C) 2015 - 2020 Petabridge, LLC <https://petabridge.com>
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Phobos.Actor.Common
+{
+    /// <summary>
+    ///     Marker interface used to indicate that this actor, message, etc always records metric events.
+    /// </summary>
+    public interface IMonitor
+    {
+    }
+}
